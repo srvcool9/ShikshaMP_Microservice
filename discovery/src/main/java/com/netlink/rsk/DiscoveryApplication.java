e13e42892f60bf990e4d287364d8a6ec68d31a87
Saurav <srv>
2024-08-27 15:52:41 +0530

    all cghanges

--- discovery/src/main/java/com/netlink/rsk/DiscoveryApplication.java
+++ discovery/src/main/java/com/netlink/rsk/DiscoveryApplication.java
@@ -8,9 +8,14 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryApplication  {
+public class DiscoveryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DiscoveryApplication.class);
 	}
 }
