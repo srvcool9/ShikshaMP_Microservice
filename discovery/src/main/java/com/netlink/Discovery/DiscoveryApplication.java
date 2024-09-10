153b157bc5aae845cdffe8dd11e75455fbc56080
SGawli <sauravgawli696@gmail.com>
2024-09-10 18:12:45 +0530

    My discovery server

--- discovery/src/main/java/com/netlink/Discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/netlink/Discovery/DiscoveryApplication.java
@@ -1,10 +1,13 @@
-package com.netlink.rsk;
+package com.netlink.Discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -13,9 +16,14 @@ public class DiscoveryApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(DiscoveryApplication.class);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
